d716c55f5470165dd1f463aac9c5765c6c270211
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2025-04-18 02:09:34 +0000

    got it working

---
+++ vite-project/src/Components/BoardGame.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function BoardGame({ onSelectSquare, board }) {
+    
+  
+    return (
+      <ol id="game-board">
+        {board.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button
+                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                    dsisabled={playerSymbol !== null}
+                  >
+                    {playerSymbol}
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+  
